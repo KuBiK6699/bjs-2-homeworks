65ffee46490c38d6f28be00b3b51dbd19b4ac6e6
KuBiK6699 <kubik6699@yandex.ru>
2022-05-06 13:53:06 +0300

    First version

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,40 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2-4*a*c;
+  if (d < 0) { 
+    return (arr);
+  } else if (d === 0) {
+    return ([(-b/(2*a))]);
+  } else if (d > 0) {
+    return ([(-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d))/(2*a)]);
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  let entryPercent = parseInt(percent) / 100;
+  let entryContribution = parseInt(contribution);
+  let entryAmount = parseInt(amount);
+
+  if (isNaN(percent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return totalAmount = `Параметр "Сумма кредита" содержит неправильное значение "${amount}"`;
+  }
+
+  let returnSumm = entryAmount - entryContribution;
+  let startDate = new Date();
+
+  let timeCreditDate = (date.getFullYear() - startDate.getFullYear()) * 12 + startDate.getMonth() - date.getMonth();
+
+  let everyMonthPay = returnSumm * ((entryPercent / 12) + ((entryPercent / 12) / (((1 + (entryPercent / 12)) ** timeCreditDate) - 1)));
+
+  totalAmount = (timeCreditDate * everyMonthPay).toFixed(2);
 
   return totalAmount;
 }
